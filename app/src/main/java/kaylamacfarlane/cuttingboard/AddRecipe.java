7f5c738398eeb92d5d9685ca3d2cfff7c72394bb
kaymac048 <kaymac456@msn.com>
2015-02-25 00:22:47 -0500

    WIP of MVP
    Was able to insert activity bar and work on add recipe page, and worked
    on starting layout but has error I don’t know how to fix. Tried to make
    navigation, but don’t really know how to do it yet.

--- app/src/main/java/kaylamacfarlane/cuttingboard/AddRecipe.java
+++ app/src/main/java/kaylamacfarlane/cuttingboard/AddRecipe.java
@@ -1,39 +1,24 @@
 package kaylamacfarlane.cuttingboard;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class AddRecipe extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddRecipe.class);
-                startActivity(intent);
-
-            }
-        });
+        setContentView(R.layout.activity_add_recipe);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_add_recipe, menu);
         return true;
     }
 
